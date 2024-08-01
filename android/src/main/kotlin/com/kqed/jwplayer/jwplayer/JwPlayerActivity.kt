c525798e572bfe1eda14ea0d617777b7ed0f0ce9
chillbrodev <jon@uptechstudio.com>
2024-08-01 11:09:10 -0700

    Invert Android Activity SupportBar to be Dark with white icons
    This allows the SupportBar to blend in with the JWPlayer more.

--- android/src/main/kotlin/com/kqed/jwplayer/jwplayer/JwPlayerActivity.kt
+++ android/src/main/kotlin/com/kqed/jwplayer/jwplayer/JwPlayerActivity.kt
@@ -1,6 +1,7 @@
 package com.kqed.jwplayer.jwplayer
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.jwplayer.pub.api.configuration.PlayerConfig
 import com.jwplayer.pub.api.media.playlists.PlaylistItem
@@ -11,6 +12,10 @@ class JwPlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_jw_player)
+
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
 
         val view = findViewById<JWPlayerView>(R.id.jwPlayerView)
         val url = intent.getStringExtra("url")
@@ -26,5 +31,12 @@ class JwPlayerActivity : AppCompatActivity() {
             .build()
 
         view.getPlayer(this).setup(config)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> finish()
+        }
+        return true
     }
 }
